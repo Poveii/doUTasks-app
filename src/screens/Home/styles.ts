42419057948471cfa3e57a40ed9c44fa51d4bed2
Pablo Gabriel <yellowtutors@gmail.com>
2024-05-15 17:00:09 -0300

    feat: Add Home component

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1A1A1A",
+  },
+  text: {
+    fontSize: 28,
+    color: "#4EA8DE",
+  }
+})
